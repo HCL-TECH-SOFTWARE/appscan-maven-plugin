a92e4df2621cffb1e28ad6d023876236cb64288d
Matthew Murphy <murphy@mmurphy.local>
2020-04-30 14:54:30 -0400

    add analyze mojo

---
+++ src/main/java/com/hcl/appscan/maven/plugin/mojos/AnalyzeMojo.java
@@ -0,0 +1,56 @@
+package com.hcl.appscan.maven.plugin.mojos;
+
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugins.annotations.Execute;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.plugins.annotations.ResolutionScope;
+
+import com.hcl.appscan.maven.plugin.Messages;
+import com.hcl.appscan.maven.plugin.auth.MavenAuthenticationProvider;
+import com.hcl.appscan.sdk.auth.IAuthenticationProvider;
+import com.hcl.appscan.sdk.error.AppScanException;
+import com.hcl.appscan.sdk.logging.Message;
+import com.hcl.appscan.sdk.scan.CloudScanServiceProvider;
+import com.hcl.appscan.sdk.scan.IScanServiceProvider;
+
+/**
+ * Generates a .irx file and submits it to HCL Appscan for analysis.
+ */
+@Mojo (name="analyze", //$NON-NLS-1$
+		requiresProject=true,
+		requiresDependencyResolution=ResolutionScope.COMPILE,
+		defaultPhase=LifecyclePhase.PACKAGE)
+@Execute( phase=LifecyclePhase.PACKAGE )
+public final class AnalyzeMojo extends SASTMojo {
+
+	/**
+	* The api key for authenticating with the service.
+	*/ 
+	@Parameter (property="appscanKey", required=true, readonly=true) //$NON-NLS-1$
+	private String appscanKey;
+	
+	/**
+	* The secret for authenticating with the service.
+	*/ 
+	@Parameter (property="appscanSecret", required=true, readonly=true) //$NON-NLS-1$
+	private String appscanSecret;
+	
+	@Override
+	protected void run() throws MojoExecutionException {
+		try {
+			getScanManager().analyze(getProgress(), getScanProperties(), getServiceProvider());
+			getProgress().setStatus(new Message(Message.INFO, Messages.getMessage("ir.analyze.success", getIrx()))); //$NON-NLS-1$
+		} catch (AppScanException  e) {
+			getProgress().setStatus(e);
+			throw new MojoExecutionException(Messages.getMessage("ir.analyze.failed", e.getLocalizedMessage())); //$NON-NLS-1$
+		}
+	}
+	
+	private IScanServiceProvider getServiceProvider() throws AppScanException {
+		IAuthenticationProvider authProvider = new MavenAuthenticationProvider(appscanKey, appscanSecret);
+		IScanServiceProvider serviceProvider = new CloudScanServiceProvider(getProgress(), authProvider);
+		return serviceProvider;
+	}
+}
