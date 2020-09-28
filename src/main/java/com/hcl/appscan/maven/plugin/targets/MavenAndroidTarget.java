7c0ef2396df009f65818634615cc4e8efd723d17
Bao Lu <bao.lu@hcl.com>
2020-09-28 14:36:07 -0400

    ASA-2607: update to the androidmanifest.xml file search

--- src/main/java/com/hcl/appscan/maven/plugin/targets/MavenAndroidTarget.java
+++ src/main/java/com/hcl/appscan/maven/plugin/targets/MavenAndroidTarget.java
@@ -7,8 +7,11 @@ package com.hcl.appscan.maven.plugin.targets;
 
 import java.io.File;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
 
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.filefilter.TrueFileFilter;
 import org.apache.maven.project.MavenProject;
 import com.hcl.appscan.maven.plugin.IMavenConstants;
 import com.hcl.appscan.maven.plugin.util.MavenUtil;
@@ -17,7 +20,6 @@ import com.hcl.appscan.sdk.scanners.sast.targets.DefaultTarget;
 public class MavenAndroidTarget extends DefaultTarget implements IMavenConstants {
 
 	private MavenProject m_project;
-	private File m_androidManifestFile = null;
 	
 	public MavenAndroidTarget(MavenProject project) {
 		m_project = project;
@@ -29,43 +31,31 @@ public class MavenAndroidTarget extends DefaultTarget implements IMavenConstants
 	
 	public File getTargetFile() {
 		File androidManifest = null;
-		try {
-			androidManifest = new File(MavenUtil.getPluginConfigurationProperty(m_project, ANDROID_KEY, ANDROID_MANIFEST_FILE));
-		}
-		catch (NullPointerException npe) {
-			// will ignore and continue so we can search for the AndroidManifest.xml file
-		}
 		
-		if(androidManifest != null && androidManifest.exists()) {
+		if (MavenUtil.getPluginConfigurationProperty(m_project, ANDROID_KEY, ANDROID_MANIFEST_FILE) != null)
+			androidManifest = new File(MavenUtil.getPluginConfigurationProperty(m_project, ANDROID_KEY, ANDROID_MANIFEST_FILE));
+		
+		if(androidManifest != null && androidManifest.isFile()) {
 			return androidManifest;
 		}
 		else {
 			androidManifest = new File(m_project.getBasedir(), ANDROID_MANIFEST);
-			if (androidManifest.exists()) {
+			if (androidManifest.isFile()) {
 				return androidManifest;
 			}
-			else {
-				File[] files = new File((m_project.getBasedir()).toString()).listFiles();
-				findAndroidManifestFile(files);
-				return m_androidManifestFile != null ? m_androidManifestFile : null; // null won't be reached because maven will throw error about missing AndroidManifest.xml file
-			}
+			Iterator<File> files = FileUtils.iterateFiles(m_project.getBasedir(), TrueFileFilter.INSTANCE, TrueFileFilter.INSTANCE);
+            while (files.hasNext()) {
+                File curr = files.next();
+                if (curr.toString().endsWith(ANDROID_MANIFEST)) {
+                    return curr;
+                }
+            }
 		}
+		return androidManifest;
 	}
 
 	@Override
 	public Map<String, String> getProperties() {
 		return new HashMap<String, String>();
-	}
-	
-	private void findAndroidManifestFile(File[] files) {
-	    for (File file : files) {
-	    	if (file.isFile() && file.toString().endsWith(ANDROID_MANIFEST)) {
-	    		m_androidManifestFile = file;
-	    		break;
-	    	}
-	    	else if (file.isDirectory()) {
-	            findAndroidManifestFile(file.listFiles());
-	        }
-	    }
 	}
 }
